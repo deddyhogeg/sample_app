d144c18e4aaafd2e88ea7c4f182d9ede628f8e4d
David Hogeg <dhogeg@yahoo.com>
2012-02-23 17:41:43 -0500

    Fixiing Signup link in layout -- David

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
 
+	def logo
+
+		image_tag("logo.png", alt: "Sample App", class: "round")
+	end
+
+
+
 	# Returns the full title on a per-page basis.
   def full_title(page_title)
     base_title = "Ruby on Rails Tutorial Sample App"
